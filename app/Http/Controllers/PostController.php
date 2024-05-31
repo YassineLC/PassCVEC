16c2848c5802762505c57fe4664107bfee4e4e12
yassine <yassine.lahmar-cherif@crous-versailles.fr>
2024-05-31 11:54:37 +0200

    fix

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,8 @@ use App\Models\Post;
 use App\Models\Attachment;
 use Illuminate\Http\Request;
 use App\Http\Requests\PostRequest;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 use App\Http\Controllers\LogementController;
 use App\Http\Controllers\PDFCheckController;
 use Illuminate\Validation\ValidationException;
@@ -13,6 +15,7 @@ use Illuminate\Validation\ValidationException;
 class PostController extends Controller
 {
     protected $logementController;
+    protected $pdfCheckController;
 
     public function __construct(LogementController $logementController, PDFCheckController $pdfCheckController)
     {
@@ -26,11 +29,10 @@ class PostController extends Controller
         return view('cvec-post-form', ['logements' => $logements]);
     }
 
-    // Méthode pour traiter la soumission du formulaire de création de post
     public function store(Request $request)
     {
         try {
-            $validator = \Validator::make($request->all(), (new PostRequest)->rules(), (new PostRequest)->messages());
+            $validator = Validator::make($request->all(), (new PostRequest)->rules(), (new PostRequest)->messages());
 
             if ($validator->fails()) {
                 throw new ValidationException($validator);
@@ -38,17 +40,13 @@ class PostController extends Controller
 
             $this->validate($request, (new PostRequest)->rules());
 
-            //Vérification de la validité du certificat CVEC
             $pdfFile = $request->file('cvec');
 
-            // Vérifiez si le fichier PDF a été correctement téléchargé
             if (!$pdfFile) {
                 throw ValidationException::withMessages(['cvec' => 'Veuillez soumettre un fichier PDF valide.']);
             }
 
-            // Obtenez le chemin temporaire du fichier PDF
             $pdfFilePath = $pdfFile->path();
-
             $apiResponse = $this->pdfCheckController->checkCVEC($pdfFilePath);
 
             if (!$apiResponse) {
@@ -58,10 +56,10 @@ class PostController extends Controller
             preg_match('/(\d{9}[A-Z]{2})/', $apiResponse['content'], $matches);
             $ine = $matches[0];
             if ($request->ine != $ine) {
-                throw ValidationException::withMessages(['ine' => 'Le numéro INE fourni n\' est pas valide.']);
+                throw ValidationException::withMessages(['ine' => 'Le numéro INE fourni n\'est pas valide.']);
             }
 
-            $is_in_residence = $request->input('is_in_residence') === 'true' ? true : false;
+            $is_in_residence = $request->input('is_in_residence') === 'true';
 
             $post = new Post;
             $post->nom = $request->nom;
@@ -75,7 +73,6 @@ class PostController extends Controller
             }
             $post->save();
 
-            // Gérer les pièces jointes et les enregistrer dans la base de données
             $this->handleAttachments($request, $post);
 
             return back()->with('success', 'La demande a bien été enregistrée.');
@@ -85,14 +82,12 @@ class PostController extends Controller
         }
     }
 
-    // Cette méthode a pour but de gérer la soumission des pièces jointes dans le formulaire
     private function handleAttachments(Request $request, Post $post)
     {
         try {
             $this->validate($request, (new PostRequest)->attachment_rules());
 
             foreach ($request->file() as $key => $file) {
-                // Déterminer le dossier de destination en fonction du type de pièce jointe
                 switch ($key) {
                     case 'scolarite':
                         $folder = 'scolarite';
@@ -118,6 +113,6 @@ class PostController extends Controller
             throw $e;
         }
     }
-
 }
+
 

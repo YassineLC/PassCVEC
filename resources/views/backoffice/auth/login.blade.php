3f75f4765021ffa855f801f64a85e8e3a6207d38
yassine <yassine.lahmar-cherif@crous-versailles.fr>
2024-10-21 15:13:16 +0200

    Adding registration (temporary)

--- resources/views/backoffice/auth/login.blade.php
+++ resources/views/backoffice/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connexion - Backoffice Crous de Versailles</title>
+    <title>Connexion - Backoffice</title>
     <style>
         @font-face {
             font-family: 'Marianne';
